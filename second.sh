0db54497fffa62e4f6b95809a1aa9144b111eb74
NPL <66727014+Npepperlinux@users.noreply.github.com>
2021-01-29 18:45:21 +0900

    Add files via upload

---
+++ second.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+firefox https://vivaldi.com
+firefox https://www.google.co.jp/chrome
+firefox https://t.co/KFhfT97OpK?amp=1
+firefox https://code.visualstudio.com
+
+read -p "Are you ready?(y/n) :"DATA
+	[yY])reboot
+	[nN])exit
+	esac
