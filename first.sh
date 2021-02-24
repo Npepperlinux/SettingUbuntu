a7e6ef2537561c945022933b3b2e33bcbd6372ed
Npepperlinux <19j31ryoga@jr-doc.shinshu-u.ac.jp>
2021-02-24 19:53:25 +0900

    'main'でコミット

--- first.sh
+++ first.sh
@@ -2,43 +2,43 @@
 
 	#This script change Ubuntu.
 	#You have to check terminal motion
-sudo apt update
-sudo apt upgrade
+echo &sudo apt update
+echo &sudo apt upgrade
 	#Install softwere from here.
-sudo apt install neofetch
-sudo apt install screenfetch
-sudo apt install sl
-sudo apt install git
-sudo apt install g++
-sudo apt install gimp
-sudo apt install vlc
-sudo apt install htop
-sudo apt install make
-sudo apt install make-guile
-sudo apt install vivaldi-stable
-sudo apt install inkscape
-sudo apt autoremove libreoffice
-sudo snap install discord
-sudo snap install libreoffice
-sudo snap install vivaldi
+echo &sudo apt install neofetch
+echo &sudo apt install screenfetch
+echo &sudo apt install sl
+echo &sudo apt install git
+echo &sudo apt install g++
+echo &sudo apt install gimp
+echo &sudo apt install vlc
+echo &sudo apt install htop
+echo &sudo apt install make
+echo &sudo apt install make-guile
+echo &sudo apt install vivaldi-stable
+echo &sudo apt install inkscape
+echo &sudo apt autoremove libreoffice
+echo &sudo snap install discord
+echo &sudo snap install libreoffice
+echo &sudo snap install vivaldi
 
 	# Install "wine" from here.
-sudo dpkg --add-architecture i386
-wget -nc https://dl.winehq.org/wine-builds/winehq.key
-sudo apt-key add winehq.key
-sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main'
+echo &sudo dpkg --add-architecture i386
+echo &wget -nc https://dl.winehq.org/wine-builds/winehq.key
+echo &sudo apt-key add winehq.key
+echo &sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main'
 	# If you use 'Ubuntu 20.10',you have to change from 'focal' to 'groovy'.	
 	# If you use 'Ubuntu 18.04',you have to change from 'focal' to 'bionic'.
-sudo apt update
-sudo apt upgrade
-sudo apt install --install-recommends winehq-stable
+echo &sudo apt update
+echo &sudo apt upgrade
+echo &sudo apt install --install-recommends winehq-stable
 	# If you want to install 'Development branch', you have to change from 'stable' to 'devel'.
 	# If you want to install 'Staging branch', you have to change from 'stable' to 'staging'.
-sudo apt install winetricks
+echo &sudo apt install winetricks
 
 echo "Shell will be FINISH soon!"
 read -p "Reboot this computer (y/n) :" 
-	[yY]) reboot
+	[yY]) echo &sudo reboot
 	[nN]) echo "You have to reboot this computer."
 	[sl]) sl
 	*) echo "Push y or n key."
--- second.sh
+++ second.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-firefox https://vivaldi.com
 firefox https://www.google.co.jp/chrome
 firefox https://t.co/KFhfT97OpK?amp=1
 firefox https://code.visualstudio.com
